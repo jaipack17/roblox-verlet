b6c06fcf90d59e75287e3392f934d567f71bd4eb
jaipack17 <74130881+jaipack17@users.noreply.github.com>
2021-09-17 22:05:31 +0530

    Create Segment.lua

---
+++ 2D Physics Engine/Segment.lua
@@ -0,0 +1,44 @@
+local Segment = {}
+Segment.__index = Segment
+
+local line = require(script.Parent.Line)
+
+function Segment.new(p1, p2, visible, name, th)
+	local self = setmetatable({
+		name = name,
+		frame = line(0, 0, 0, 0, script.Parent.Parent.Canvas, 4),
+		point1 = p1,
+		point2 = p2,
+		restLength = (p2.pos - p1.pos).magnitude,
+		visible = visible,
+		th = th or 3
+	}, Segment)
+	
+	return self	
+end
+
+function Segment:Simulate()
+	local currentLength = (self.point2.pos - self.point1.pos).magnitude
+	local lengthDifference = self.restLength - currentLength
+	local offsetPercent = (lengthDifference / currentLength) / 2
+	
+	local direction = self.point2.pos 
+	direction -= self.point1.pos 
+	direction *= offsetPercent
+	
+	if not self.point1.snap then
+		self.point1.pos -= direction
+	end
+	
+	if not self.point2.snap then
+		self.point2.pos += direction
+	end
+end
+
+function Segment:Draw()
+	if self.visible then
+		line(self.point1.pos.x, self.point1.pos.y, self.point2.pos.x, self.point2.pos.y, script.Parent.Parent.Canvas, self.th, self.frame)
+	end
+end
+
+return Segment
